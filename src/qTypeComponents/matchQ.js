8194e219b4e9c9efd580b1a3fdfe6bf17c881ae7
Samir70 <sbetmouni@talktalk.net>
2021-09-27 06:36:07 +0100

    enable matching of pairs in match-q

--- src/qTypeComponents/matchQ.js
+++ src/qTypeComponents/matchQ.js
@@ -7,35 +7,30 @@ Vue.component('matchQ', {
         qData: Object
     },
     data: function () {
-        return { userAnswer: '', userWasCorrect: false }
+        return {
+            firstTile: '', userWasCorrect: false,
+            cards: shuffle([].concat(...this.qData.pairs).map((x, i) => {
+                return {
+                    text: x, id: i, class: 'mcq-option'
+                }
+            }))
+        }
     },
     computed: {
         qText: function () { return this.qData.question },
-        pairs: function () { 
+        pairs: function () {
             let out = {}
             for (let [a, b] of this.qData.pairs) {
                 out[a] = b; out[b] = a;
             }
             console.log(out)
             return out
-        },
-        cards: function () {
-            let out = [];
-            for (let [a, b] of this.qData.pairs) {
-                out.push(a); out.push(b)
-            }
-            console.log(out)
-            return shuffle(out).map((x, i) => {
-                return {
-                    text: x, id: i, class: 'mcq-option'
-                }
-            })
         }
     },
     methods: {
         checkAnswer: function () {
             let mark = '';
-            let blandFB = 'The answer is '+this.ans
+            let blandFB = 'The answer is ' + this.ans
             if (true/**test for correct */) {
                 this.userWasCorrect = true;
                 mark = 'Correct! '
@@ -44,7 +39,28 @@ Vue.component('matchQ', {
                 mark = 'Wrong! '
             }
             let extra = (this.qData.feedback || blandFB)
-            return {status: this.userWasCorrect, mark, extra}
+            return { status: this.userWasCorrect, mark, extra }
+        },
+        updateUserAnswer: function (tile) {
+            console.log('react to click on', tile)
+            if (this.firstTile === '') {
+                this.firstTile = tile;
+            } else {
+                if (this.pairs[tile] === this.firstTile) {
+                    console.log(tile, this.firstTile, 'is correct')
+                    for (let c of this.cards) {
+                        if (c.class === 'mcq-option solved') { continue }
+                        c.class = c.text === tile || c.text === this.firstTile ?
+                        'mcq-option solved' : 'mcq-option'
+                    }
+                }
+                this.firstTile = ''
+                tile = ''
+            }
+            for (let c of this.cards) {
+                if (c.class === 'mcq-option solved') { continue }
+                c.class = c.text === tile ? 'mcq-option selected' : 'mcq-option'
+            }
         }
     },
     template: `
@@ -56,7 +72,7 @@ Vue.component('matchQ', {
                 v-bind:key="item.id" 
                 v-bind:class="item.class"
                 v-on:click="updateUserAnswer(item.text)">{{item.text}} 
-        </div>
+            </div>
             </div>
             <button v-on:click="checkAnswer; $emit('user-answered', checkAnswer())" >Check Answer</button>
         </div>
